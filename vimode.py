e04b32fdf923cc4eb2b8ee98e291905693a25bd9
Sebastien Helleu <flashcode@flashtux.org>
2020-07-22 16:07:39 +0200

    Make call to bar_new compatible with WeeChat >= 2.9

--- vimode.py
+++ vimode.py
@@ -45,7 +45,7 @@ import weechat
 
 SCRIPT_NAME = "vimode"
 SCRIPT_AUTHOR = "GermainZ <germanosz@gmail.com>"
-SCRIPT_VERSION = "0.7"
+SCRIPT_VERSION = "0.8"
 SCRIPT_LICENSE = "GPL3"
 SCRIPT_DESC = ("Add vi/vim-like modes and keybindings to WeeChat.")
 
@@ -2208,9 +2208,14 @@ if __name__ == "__main__":
     weechat.bar_item_new("cmd_completion", "cb_cmd_completion", "")
     weechat.bar_item_new("vi_buffer", "cb_vi_buffer", "")
     weechat.bar_item_new("line_numbers", "cb_line_numbers", "")
-    weechat.bar_new("vi_line_numbers", "on", "0", "window", "", "left",
-                    "vertical", "vertical", "0", "0", "default", "default",
-                    "default", "0", "line_numbers")
+    if int(VERSION) >= 0x02090000:
+        weechat.bar_new("vi_line_numbers", "on", "0", "window", "", "left",
+                        "vertical", "vertical", "0", "0", "default", "default",
+                        "default", "default", "0", "line_numbers")
+    else:
+        weechat.bar_new("vi_line_numbers", "on", "0", "window", "", "left",
+                        "vertical", "vertical", "0", "0", "default", "default",
+                        "default", "0", "line_numbers")
     weechat.hook_config("plugins.var.python.%s.*" % SCRIPT_NAME, "cb_config",
                         "")
     weechat.hook_signal("key_pressed", "cb_key_pressed", "")
